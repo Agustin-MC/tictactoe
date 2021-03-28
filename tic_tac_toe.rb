19966cca36b93dff7bc50255044b946c92b90d93
Agustin <agustin4u@tamu.edu>
2021-03-27 22:37:10 -0500

    column check

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -14,37 +14,22 @@ class TicTacToe
           return "x"
         end
       end
+
+      #col checks
+      col = @board.map {|row| row[index]}
+      unless col.include?(" ")
+        unless col.include?("x")
+          return "o"
+        end
+        unless col.include?("o")
+          return "x"
+        end
+      end
     end
 
     row1 = @board[0]
     row2 = @board[1]
     row3 = @board[2]
-
-    # column checks
-
-    if row1[0] == "o" && row2[0] == "o" && row3[0] == "o"
-      return "o"
-    end
-
-    if row1[1] == "o" && row2[1] == "o" && row3[1] == "o"
-      return "o"
-    end
-
-    if row1[2] == "o" && row2[2] == "o" && row3[2] == "o"
-      return "o"
-    end
-
-    if row1[0] == "x" && row2[0] == "x" && row3[0] == "x"
-      return "x"
-    end
-
-    if row1[1] == "x" && row2[1] == "x" && row3[1] == "x"
-      return "x"
-    end
-
-    if row1[2] == "x" && row2[2] == "x" && row3[2] == "x"
-      return "x"
-    end
 
     # diagonal checks
 
@@ -64,6 +49,7 @@ class TicTacToe
       return "x"
     end
 
+    #unfinished check
     @board.each do |row|
       if row.include? " "
         return "unfinished"
