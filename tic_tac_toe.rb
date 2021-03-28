d005d5d8e58147ab6ebad5ebdd08e1194c2295a2
Agustin <agustin4u@tamu.edu>
2021-03-27 23:46:05 -0500

    refactoring

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -3,52 +3,35 @@ class TicTacToe
     @board = board
   end
 
+  def checkArray(array)
+    unless array.include?(" ")
+      unless array.include?("x")
+        return "o"
+      end
+      unless array.include?("o")
+        return "x"
+      end
+    end
+  end
+
   def winner
     forwardDiag = Array.new
     backwardDiag = Array.new
     @board.each_with_index do |row,index|
       # row checks
-      unless row.include?(" ")
-        unless row.include?("x")
-          return "o"
-        end
-        unless row.include?("o")
-          return "x"
-        end
-      end
+      return checkArray(row) unless checkArray(row).nil?
 
       #col checks
       col = @board.map {|row| row[index]}
-      unless col.include?(" ")
-        unless col.include?("x")
-          return "o"
-        end
-        unless col.include?("o")
-          return "x"
-        end
-      end     
+      return checkArray(col) unless checkArray(col).nil?
 
       #diag checks
       forwardDiag.push(@board[index][index])
       backwardDiag.push(@board[index][-(index+1)]) 
     end
 
-    unless forwardDiag.include?(" ")
-      unless forwardDiag.include?("x")
-        return "o"
-      end
-      unless forwardDiag.include?("o")
-        return "x"
-      end
-    end
-    unless backwardDiag.include?(" ")
-      unless backwardDiag.include?("x")
-        return "o"
-      end
-      unless backwardDiag.include?("o")
-        return "x"
-      end
-    end   
+    return checkArray(forwardDiag) unless checkArray(forwardDiag).nil?
+    return checkArray(backwardDiag) unless checkArray(backwardDiag).nil?
 
     #unfinished check
     @board.each do |row|
