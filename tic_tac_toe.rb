8a66c6490a6513005c41fa5b53f881a1484d0939
Agustin <agustin4u@tamu.edu>
2021-03-27 21:45:37 -0500

    row check regardless of size

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -4,35 +4,21 @@ class TicTacToe
   end
 
   def winner
+    @board.each_with_index do |row,index|
+      # row checks
+      unless row.include?(" ")
+        unless row.include?("x")
+          return "o"
+        end
+        unless row.include?("o")
+          return "x"
+        end
+      end
+    end
+
     row1 = @board[0]
     row2 = @board[1]
     row3 = @board[2]
-
-    # row checks
-
-    if row1[0] == "o" && row1[1] == "o" && row1[2] == "o"
-      return "o"
-    end
-
-    if row2[0] == "o" && row2[1] == "o" && row2[2] == "o"
-      return "o"
-    end
-
-    if row3[0] == "o" && row3[1] == "o" && row3[2] == "o"
-      return "o"
-    end
-
-    if row1[0] == "x" && row1[1] == "x" && row1[2] == "x"
-      return "x"
-    end
-
-    if row2[0] == "x" && row2[1] == "x" && row2[2] == "x"
-      return "x"
-    end
-
-    if row3[0] == "x" && row3[1] == "x" && row3[2] == "x"
-      return "x"
-    end
 
     # column checks
 
@@ -78,8 +64,8 @@ class TicTacToe
       return "x"
     end
 
-    @board.each do |n|
-      if n.include? " "
+    @board.each do |row|
+      if row.include? " "
         return "unfinished"
       end
     end
